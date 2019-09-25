bc2111857e747ee2b8c5fc38e334ab7a57102942
ChandanShaw7 <chandan.shaw@kuliza.com>
2019-09-25 20:14:34 +0530

    adding upto asynchronous call

--- src/main/java/com/example/demo/SpringRestApiApplication.java
+++ src/main/java/com/example/demo/SpringRestApiApplication.java
@@ -1,15 +1,42 @@
 package com.example.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
 
 @SpringBootApplication
 @EnableJpaAuditing
+//@EnableAsync
 public class SpringRestApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringRestApiApplication.class, args);
 	}
 
+//	private static final Logger log = LoggerFactory.getLogger(SpringRestApiApplication.class);
+//	@Bean
+//	public Executor taskExecutor() {
+//
+////		LOGGER.debug("inside task Executor");
+//		log.info("inside task Executor");
+//		ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+//		executor.setCorePoolSize(2);
+//		executor.setMaxPoolSize(2);
+//		executor.setQueueCapacity(500);
+//		executor.setThreadNamePrefix("Getrestdata-");
+//		executor.initialize();
+//		log.info(executor.getThreadNamePrefix());
+//		return executor;
+//	}
+
 }
+
+
---
+++ src/main/java/com/example/demo/repository/AnswerRepository.java
@@ -0,0 +1,17 @@
+package com.example.demo.repository;
+
+import com.example.demo.model.Answers;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AnswerRepository extends CrudRepository<Answers, Integer>{
+
+    @Query(value ="SELECT * FROM Answers a WHERE a.q_id = ?1", nativeQuery = true)
+    Iterable<Answers> findByQuestionId(int q_id);
+
+    @Query(value ="SELECT * FROM Answers a WHERE a.u_id = ?1", nativeQuery = true)
+    Iterable<Answers> findByUserId(int u_id);
+
+}
